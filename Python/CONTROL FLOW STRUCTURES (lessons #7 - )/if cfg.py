c2eae3c791701fa85458716fbfa5baab4dee1ae9
SUPERNOOB20 <andycia802@gmail.com>
2026-04-13 00:44:04 -0300

    Adds *some* Lesson #7 activities :3

---
+++ Python/CONTROL FLOW STRUCTURES (lessons #7 - )/if cfg.py
@@ -0,0 +1,10 @@
+# Disco example
+# (Ejemplo boliche)
+
+print("Hey! How old are you?\n")
+my_age = input()
+
+if (my_age < 18):
+    print("You shall not pass!")
+else:
+    print("Please, come in! :3")
